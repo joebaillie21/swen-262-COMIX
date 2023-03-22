6de423346b06ae3343f6a2c634ed57f7dfb2cd11
Angela Ngo <ngo.angela0@gmail.com>
2023-03-22 17:07:34 -0400

    this is the schema but not entirely done

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,4 +1,4 @@
-
+DROP TABLE IF EXISTS comics 
 -- CREATE TABLE users(
 --     id SERIAL PRIMARY KEY, 
 --     username TEXT NOT NULL DEFAULT '',
@@ -10,11 +10,6 @@ CREATE TABLE comics(
     series_name TEXT NOT NULL, 
     issue_num INTEGER NOT NULL, 
     full_title TEXT NOT NULL, 
-    
-);
 
-CREATE TABLE publishers(
-    id SERIAL PRIMARY KEY,
-    comic_id INTEGER NOT NULL,
-    pub_name TEXT NOT NULL
+    
 );
