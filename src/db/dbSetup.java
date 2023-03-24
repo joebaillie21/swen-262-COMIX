fedb0b6737262b5e8f077b9462e4ed67d6652073
jjb7866 <jjb7866@rit.edu>
2023-03-24 17:16:44 -0400

    Semantic changes

--- src/db/dbSetup.java
+++ src/db/dbSetup.java
@@ -11,8 +11,10 @@ public class dbSetup {
 
             Connection con = dbConnection.getConnection();
             PreparedStatement create = con.prepareStatement(
-                    "CREATE TABLE IF NOT EXISTS COMIX(id SERIAL PRIMARY KEY,  series_title TEXT NOT NULL, volume_number INT NOT NULL, issue_number INT NOT NULL, publication_date DATE, author TEXT, publisher_id INT, principle_character TEXT,  description TEXT, value FLOAT, grade INT)");
+                    "CREATE TABLE IF NOT EXISTS comics(id SERIAL PRIMARY KEY,  series_title TEXT NOT NULL, volume_number INT NOT NULL, issue_number INT NOT NULL, publication_date DATE, author TEXT, publisher_id INT, principle_character TEXT,  description TEXT, value FLOAT, grade INT)");
             create.executeUpdate();
+            PreparedStatement delete = con.prepareStatement("DROP TABLE IF EXISTS comix");
+            delete.executeUpdate();
 
         } catch (Exception e) {
             System.out.println(e);
