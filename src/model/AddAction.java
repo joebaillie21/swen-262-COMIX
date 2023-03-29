9ead7dab1ed13f705428089da6960d4e8931cdcd
Angela Ngo <ngo.angela0@gmail.com>
2023-03-29 00:27:06 -0400

    made the add action

--- src/model/AddAction.java
+++ src/model/AddAction.java
@@ -1,25 +1,32 @@
 package model;
 public class AddAction implements Action {
     private PersonalCollection pc ;
+    private ComicBook comic; 
 
     public AddAction(PersonalCollection pc){
         this.pc = pc; 
+
     }
     @Override
     public void execute(ComicBook comic) {
         pc.add((ComicBookComponent)comic); 
+        this.comic = comic; 
     }
 
     @Override
+    /**
+     * The adding action is reversible so it will be 
+     */
     public boolean isReversible() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isReversible'");
+        return true;
     }
 
     @Override
+    /**
+     * this unexecutes the specific add command
+     */
     public void unexecute() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'unexecute'");
+        pc.remove((ComicBookComponent)comic); 
     }
     
 }
