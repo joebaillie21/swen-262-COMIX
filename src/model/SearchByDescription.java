73190ccca9f88f561608c9c800fc0bcd996eaa48
Angela Ngo <ngo.angela0@gmail.com>
2023-04-03 18:57:26 -0400

    did search by description + principle character + author

--- src/model/SearchByDescription.java
+++ src/model/SearchByDescription.java
@@ -4,10 +4,45 @@ import java.util.ArrayList;
 
 public class SearchByDescription implements SearchStrategy {
     private String toBeSearched;
+    private PersonalCollection personalCollection; 
+    final int NUM_RESULTS = 10;
+
+    public SearchByDescription(PersonalCollection personalCollection){
+        this.personalCollection = personalCollection; 
+    }
 
     @Override
-    public ArrayList<ComicBook> algorithm(String toBeSearched) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'algorithm'");
+    public ArrayList<ComicBook> algorithm(String toBeSearched, boolean isSearchDb) {
+        ArrayList<ComicBook> comics = new ArrayList<>();
+        
+        if(isSearchDb == true){
+            comics = searchOnDb(toBeSearched);
+        }else if(isSearchDb == false){
+            comics = searchOnPC(toBeSearched); 
+        } 
+        return comics;
+    }
+
+    private ArrayList<ComicBook> searchOnDb(String toBeSearched){
+        // joe implementation
+    }
+
+    private ArrayList<ComicBook> searchOnPC(String toBeSearched){
+        ArrayList<ComicBook> comics = new ArrayList<>(); 
+        ArrayList<ComicBook> pc = personalCollection.getPersonalCollection(); 
+        int currCount = 0; 
+        
+        for(int i = 0; i < pc.size(); i++){
+            ComicBookComponent comic = (ComicBookComponent) pc.get(i);
+            if(comic.getDescription().equals(toBeSearched) && currCount != NUM_RESULTS){
+                comics.add(pc.get(i)); 
+                currCount++;
+            }else if(currCount == NUM_RESULTS){
+                return comics; 
+            }
+        
+        }
+        // return the ones even if it doesn reach the max NUM_COUNT
+        return comics; 
     }
 }
--- src/model/SearchByPrincipleCharacter.java
+++ src/model/SearchByPrincipleCharacter.java
@@ -1,13 +1,66 @@
 package model;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class SearchByPrincipleCharacter implements SearchStrategy {
     private String toBeSearched;
+    private PersonalCollection personalCollection; 
+    final int NUM_RESULTS = 10;
 
-    @Override
-    public ArrayList<ComicBook> algorithm(String toBeSearched) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'algorithm'");
+    public SearchByPrincipleCharacter(PersonalCollection personalCollection){
+        this.personalCollection = personalCollection; 
     }
+
+    /**
+     * Assuming that the toBeSearched needs to be parsed but they need to be split with commas 
+     */
+    @Override
+    public ArrayList<ComicBook> algorithm(String toBeSearched, boolean isSearchDb) {
+        ArrayList<ComicBook> comics = new ArrayList<>();
+        String [] characters = toBeSearched.split(",");
+        
+        if(isSearchDb == true){
+            comics = searchOnDb(toBeSearched);
+        }else if(isSearchDb == false){
+            comics = searchOnPC(characters); 
+        } 
+        return comics;
+    }
+
+    private ArrayList<ComicBook> searchOnPC(String [] toBeSearched){
+        
+        ArrayList<ComicBook> comics = new ArrayList<>(); 
+        ArrayList<ComicBook> pc = personalCollection.getPersonalCollection(); 
+        int currCount = 0; 
+        
+        boolean isMatch = false; // reset everytime new comic iterate 
+        for(int i = 0; i < pc.size(); i++){
+            ComicBookComponent comic = (ComicBookComponent) pc.get(i);
+            ArrayList<String> characters = comic.getPrincipleCharacter();
+            
+            for(int k = 0; k < toBeSearched.length; k++){
+                if(!characters.get(k).equals(toBeSearched[k])){
+                    isMatch = false;
+                    break; 
+                }
+            }
+
+            if(isMatch == true && currCount != NUM_RESULTS ){
+                currCount++;
+                isMatch = false; // reset 
+                comics.add(comic); 
+            }else if(currCount == NUM_RESULTS){ // when the max results reached 
+                return comics; 
+            }
+
+        }
+        // return the ones even if it doesn reach the max NUM_COUNT
+        return comics; 
+    }
+
+    private ArrayList<ComicBook> searchOnDb(String toBeSearched){
+        // joe implementation 
+    }
+
 }
