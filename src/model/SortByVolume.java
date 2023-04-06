d4268349f676b9bd12ce30dee945cb05da704598
Peyton Wagner <paw7341@g.rit.edu>
2023-04-06 12:19:44 -0400

    sort by volume for pc

--- src/model/SortByVolume.java
+++ src/model/SortByVolume.java
@@ -11,8 +11,39 @@ public class SortByVolume implements SortStrategy{
 
     @Override
     public ArrayList<ComicBook> algorithm(String toBeSorted, boolean isSortDb) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'algorithm'");
+        ArrayList<ComicBook> comics = new ArrayList<>();
+
+        if (isSortDb == true) {
+            comics = sortOnDb(toBeSorted);
+        } else if (isSortDb == false) {
+            comics = sortOnPC();
+        }
+
+        return comics;
     }
+
+    private ArrayList<ComicBook> sortOnDb(String toBeSorted){
+        // joe
+        return null;
+    }
+
+    private ArrayList<ComicBook> sortOnPC(){
+
+        ArrayList<ComicBook> pc = (ArrayList<ComicBook>)personalCollection.getPersonalCollection();
+        Collections.sort(pc, SortByVolume.cbComparator);
+
+        return null;
+    }
+
+    public static Comparator<ComicBook> cbComparator = new Comparator<ComicBook>(){
+        @Override
+        public int compare(ComicBook o1, ComicBook o2) {
+            ComicBookComponent comic1 = (ComicBookComponent) o1;
+            ComicBookComponent comic2 = (ComicBookComponent) o2;
+            return String.valueOf(comic1.getVolNum()).compareTo(String.valueOf(comic2.getVolNum()));
+        }
+    };
+
+
     
 }
