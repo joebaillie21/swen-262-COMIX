06130928007777d5a368a928c2cd370224d11b69
Peyton Wagner <paw7341@g.rit.edu>
2023-04-06 12:14:19 -0400

    sort by publication date for pc

--- src/model/SortByPublicationDate.java
+++ src/model/SortByPublicationDate.java
@@ -1,6 +1,10 @@
 package model;
 import java.util.*;
 
+/**
+ * @Author Joe and Peyton
+ */
+
 public class SortByPublicationDate implements SortStrategy{
 
     private PersonalCollection personalCollection;
@@ -11,8 +15,39 @@ public class SortByPublicationDate implements SortStrategy{
 
     @Override
     public ArrayList<ComicBook> algorithm(String toBeSorted, boolean isSortDb) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'algorithm'");
+        ArrayList<ComicBook> comics = new ArrayList<>();
+
+        if (isSortDb == true) {
+            comics = sortOnDb(toBeSorted);
+        } else if (isSortDb == false) {
+            comics = sortOnPC();
+        }
+
+        return comics;
     }
+
+    private ArrayList<ComicBook> sortOnDb(String toBeSorted){
+        // joe
+        return null;
+    }
+
+    private ArrayList<ComicBook> sortOnPC(){
+
+        ArrayList<ComicBook> pc = (ArrayList<ComicBook>)personalCollection.getPersonalCollection();
+        Collections.sort(pc, SortByPublicationDate.cbComparator);
+
+        return null;
+    }
+
+    public static Comparator<ComicBook> cbComparator = new Comparator<ComicBook>(){
+        @Override
+        public int compare(ComicBook o1, ComicBook o2) {
+            ComicBookComponent comic1 = (ComicBookComponent) o1;
+            ComicBookComponent comic2 = (ComicBookComponent) o2;
+            return String.valueOf(comic1.getPubDate()).compareTo(String.valueOf(comic2.getPubDate()));
+        }
+    };
+
+
     
 }
