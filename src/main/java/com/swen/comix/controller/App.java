a85834933b5de14970547741cca43608272b9b61
AshRoushinko <Ashroushinko@gmail.com>
2023-04-10 22:05:31 -0400

    Added skeleton code for searching, error still present

--- src/main/java/com/swen/comix/controller/App.java
+++ src/main/java/com/swen/comix/controller/App.java
@@ -12,12 +12,18 @@ import com.swen.comix.model.ComixLogin;
 import com.swen.comix.model.ComixMediator;
 import com.swen.comix.model.Guest;
 import com.swen.comix.model.PersonalCollection;
+import com.swen.comix.model.SearchByAuthor;
+import com.swen.comix.model.SearchByDescription;
+import com.swen.comix.model.SearchByPrincipleCharacter;
+import com.swen.comix.model.SearchBySeriesTitle;
 import com.swen.comix.model.SignedInUser;
 import com.swen.comix.model.User;
 import com.swen.comix.persistence.UserDAO;
 import com.swen.comix.persistence.UserFileDAO;
 import com.swen.comix.view.PTUI;
-
+/*
+ * @Author Ash Roushinko
+ */
 public class App {
     private User user;
     private Guest guest;
@@ -27,7 +33,7 @@ public class App {
     private Scanner input;
     private Boolean running;
     private String username, password, searchResult;
-    private PersonalCollection guestPC;
+    private PersonalCollection collection;
 
     public App() throws IOException{
         init();
@@ -43,24 +49,18 @@ public class App {
                 String newInput = input.nextLine();
                 if(newInput.equals("1")){
                     view.setCommand(Command.SIGNIN);
-                    view.handleCommand();
                 }
                 else if(newInput.equals("2")){
                     view.setCommand(Command.SIGNUP);
-                    view.handleCommand();
                 }
                 else if(newInput.equals("3")){
                     view.setCommand(Command.SEARCHDATABASE);
-                    view.handleCommand();
                 }
                 else if(newInput.equals("4")){
                     view.setCommand(Command.BROWSEOTHERCOLLECTION);
-                    view.handleCommand();
                 }
                 else if(newInput.equals("5")){
                     view.setCommand(Command.CLOSING);
-                    view.handleCommand();
-                    running = false;
                 }
                 else{
                    view.setCommand(Command.ERROR);
@@ -68,6 +68,7 @@ public class App {
                    view.setCommand(Command.GUEST);
                    view.handleCommand();
                 }
+                view.handleCommand();
             }
 
             // - SIGNIN
@@ -80,6 +81,7 @@ public class App {
                 this.password = input.nextLine();
                 try{
                     this.user = mediator.login(this.username, this.password);
+                    this.collection = this.user.getPersonalCollection();
                     view.setCommand(Command.SIGNINCOMPLETE);
                     view.handleCommand();
                     view.setCommand(Command.SIGNEDINUSER);
@@ -104,7 +106,7 @@ public class App {
                 this.password = input.nextLine();
                 try {
                     this.user = guest.createAccount(this.username, this.password);
-                    PersonalCollection collection = new PersonalCollection(this.username);
+                    this.collection = new PersonalCollection(this.username);
                     this.user.setCollection(collection);
                     view.setCommand(Command.SIGNINCOMPLETE);
                     view.handleCommand();
@@ -151,51 +153,87 @@ public class App {
             else if(view.getCommand().equals(Command.SIGNEDINUSER)){
                 String commandNum = input.nextLine();
                 if(commandNum.equals("1")){
-                    view.setCommand(Command.SEARCHDATABASE);
-                    view.handleCommand();
+                    view.setCommand(Command.SEARCHTYPEDATABASE);
                 }
                 else if(commandNum.equals("2")){
                     view.setCommand(Command.SEARCHCOLLECTION);
-                    view.handleCommand();
                 }
                 else if(commandNum.equals("3")){
                     view.setCommand(Command.ADDTOCOLLECTION);
-                    view.handleCommand();
                 }
                 else if(commandNum.equals("4")){
                     view.setCommand(Command.REMOVEFROMCOLLECTION);
-                    view.handleCommand();
                 }
                 else if(commandNum.equals("5")){
                     view.setCommand(Command.EDITMARKSELECTION);
-                    view.handleCommand();
                 }
                 else if(commandNum.equals("6")){
                     view.setCommand(Command.UNDO);
-                    view.handleCommand();
                 }
                 else if(commandNum.equals("7")){
                     view.setCommand(Command.REDO);
-                    view.handleCommand();
                 }
                 else if(commandNum.equals("8")){
                     view.setCommand(Command.IMPORTEXPORT);
-                    view.handleCommand();
                 }
                 else if(commandNum.equals("9")){
                     view.setCommand(Command.CLOSING);
+                }
+                else{
+                    view.setCommand(Command.ERROR);
+                    view.handleCommand();
+                    view.setCommand(Command.SIGNEDINUSER);
                     view.handleCommand();
                 }
+                view.handleCommand();
             }
 
-            // - BROWSE DATABASE
-            else if(view.getCommand().equals(Command.SEARCHDATABASE)) {
-                
+            // - CHANGE SEARCH TYPE
+            else if(view.getCommand().equals(Command.SEARCHTYPEDATABASE) || view.getCommand().equals(Command.SEARCHTYPECOLLECTION)) {
+                String commandNum = input.nextLine();
+                Command newCommand = Command.SEARCHDATABASE;
+                if(!view.getCommand().equals(Command.SEARCHTYPEDATABASE)){
+                    newCommand = Command.SEARCHCOLLECTION;
+                }
+            
+                if(commandNum.equals("1")){
+                    this.user.setSearchStrategy(new SearchBySeriesTitle());
+                    view.setCommand(newCommand);
+                }
+                else if(commandNum.equals("2")){
+                    this.user.setSearchStrategy(new SearchByPrincipleCharacter());
+                    view.setCommand(newCommand);
+                }
+                else if(commandNum.equals("3")){
+                    this.user.setSearchStrategy(new SearchByAuthor());
+                    view.setCommand(newCommand);
+                }
+                else if(commandNum.equals("4")){
+                    this.user.setSearchStrategy(new SearchByDescription());
+                    view.setCommand(newCommand);
+                }
+                else if(commandNum.equals("5")){
+                    view.setCommand(Command.SIGNEDINUSER);
+                }
+                else{
+                    view.setCommand(Command.ERROR);
+                    view.handleCommand();
+                    view.setCommand(Command.SIGNEDINUSER);
+                    view.handleCommand();
+                }
+                view.handleCommand();
             }
 
-            // - SEARCH PERSONAL COLLECTION
-            else if(view.getCommand().equals(Command.SEARCHCOLLECTION)){
+            // - SEARCH
+            else if(view.getCommand().equals(Command.SEARCHDATABASE) || view.getCommand().equals(Command.SEARCHCOLLECTION)){
+                boolean database = true;
+                if(!view.getCommand().equals(Command.SEARCHDATABASE)){
+                    database = false;
+                }
 
+                String search = input.nextLine();
+                this.user.search(search);
+                //this.user.search(search, database);
             }
 
             // - ADD TO PERSONAL COLLECTION
@@ -214,12 +252,12 @@ public class App {
             }
 
             // - UNDO COMMAND
-            else if(view.getCommand().equals(Command.EDITMARKSELECTION)){
+            else if(view.getCommand().equals(Command.UNDO)){
 
             }
 
             // - REDO COMMAND
-            else if(view.getCommand().equals(Command.EDITMARKSELECTION)){
+            else if(view.getCommand().equals(Command.REDO)){
 
             }
 
@@ -237,7 +275,7 @@ public class App {
     public void init() throws IOException{
         ObjectMapper mockMapper = new ObjectMapper();
         //mockMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        this.userDAO = new UserFileDAO("src/data/temp.json", mockMapper);
+        this.userDAO = new UserFileDAO("src/data/users.json", mockMapper);
         this.mediator = new ComixLogin(this.userDAO);
         this.guest = new Guest(mediator);
         this.view = new PTUI();
