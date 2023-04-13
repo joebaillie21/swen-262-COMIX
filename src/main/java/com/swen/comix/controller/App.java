a1ed063eb15490cc78f7631ead06404ba92ab577
AshRoushinko <Ashroushinko@gmail.com>
2023-04-12 20:59:38 -0400

    changed if-else statements to switch statements

--- src/main/java/com/swen/comix/controller/App.java
+++ src/main/java/com/swen/comix/controller/App.java
@@ -43,238 +43,198 @@ public class App {
         running = true;
         while(running){
             input = new Scanner(System.in);
-
-            //GUEST COMMANDS
-            if(view.getCommand().equals(Command.GUEST)){
-                String newInput = input.nextLine();
-                if(newInput.equals("1")){
-                    view.setCommand(Command.SIGNIN);
-                }
-                else if(newInput.equals("2")){
-                    view.setCommand(Command.SIGNUP);
-                }
-                else if(newInput.equals("3")){
-                    view.setCommand(Command.SEARCHDATABASE);
-                }
-                else if(newInput.equals("4")){
-                    view.setCommand(Command.BROWSEOTHERCOLLECTION);
-                }
-                else if(newInput.equals("5")){
-                    view.setCommand(Command.CLOSING);
-                }
-                else{
-                   view.setCommand(Command.ERROR);
-                   view.handleCommand();
-                   view.setCommand(Command.GUEST);
-                   view.handleCommand();
-                }
-                view.handleCommand();
-            }
-
-            // - SIGNIN
-            else if(view.getCommand().equals(Command.SIGNIN)){
-                this.username = input.nextLine();
-                view.setCommand(Command.SIGNINPASSWORD);
-                view.handleCommand();
-            }
-            else if(view.getCommand().equals(Command.SIGNINPASSWORD)){
-                this.password = input.nextLine();
-                try{
-                    this.user = mediator.login(this.username, this.password);
-                    this.collection = this.user.getPersonalCollection();
-                    view.setCommand(Command.SIGNINCOMPLETE);
-                    view.handleCommand();
-                    view.setCommand(Command.SIGNEDINUSER);
-                    view.handleCommand();
-                }
-                catch(IllegalArgumentException exception){
-                    view.setCommand(Command.ERROR);
-                    view.handleCommand();
-                    view.setCommand(Command.GUEST);
-                    view.handleCommand();
-                }
+            switch(view.getCommand()){
                 
-            }
+                case GUEST:
+                    String newInput = input.nextLine();
+                    switch (newInput){
+                        case "1":
+                            view.setCommand(Command.SIGNIN);
+                            break;
+                        case "2":
+                            view.setCommand(Command.SIGNUP);
+                            break;
+                        case "3":
+                            view.setCommand(Command.SEARCHDATABASE);
+                            break;
+                        case "4":
+                            view.setCommand(Command.BROWSEOTHERCOLLECTION);
+                            break;
+                        case "5":
+                            view.setCommand(Command.CLOSING);
+                            break;
+                        default:
+                            view.setCommand(Command.ERROR);
+                            view.handleCommand();
+                            view.setCommand(Command.GUEST);
+                    }
+                    break;
 
-            // - SIGNUP
-            else if(view.getCommand().equals(Command.SIGNUP)){
-                this.username = input.nextLine();
-                view.setCommand(Command.SIGNUPPASSWORD);
-                view.handleCommand();
-            }
-            else if(view.getCommand().equals(Command.SIGNUPPASSWORD)){
-                this.password = input.nextLine();
-                try {
-                    this.user = guest.createAccount(this.username, this.password);
-                    this.collection = new PersonalCollection(this.username);
-                    this.user.setCollection(collection);
-                    view.setCommand(Command.SIGNINCOMPLETE);
-                    view.handleCommand();
-                    view.setCommand(Command.SIGNEDINUSER);
-                    view.handleCommand();
-                } catch (Exception e) {
-                    view.setCommand(Command.NEWACCOUNTERROR);
-                    view.handleCommand();
-                    view.setCommand(Command.GUEST);
-                    view.handleCommand();
-                }
-            }
+                case SIGNIN:
+                    this.username = input.nextLine();
+                    view.setCommand(Command.SIGNINPASSWORD);
+                    break;
+                case SIGNINPASSWORD:
+                    this.password = input.nextLine();
+                    try{
+                        this.user = mediator.login(this.username, this.password);
+                        this.collection = this.user.getPersonalCollection();
+                        view.setCommand(Command.SIGNINCOMPLETE);
+                        view.handleCommand();
+                        view.setCommand(Command.SIGNEDINUSER);
+                    }
+                    catch(IllegalArgumentException exception){
+                        view.setCommand(Command.ERROR);
+                        view.handleCommand();
+                        view.setCommand(Command.GUEST);
+                    }
+                    break;
 
-            // - BROWSE COLLECTION GUEST
-            else if(view.getCommand().equals(Command.BROWSEOTHERCOLLECTION)){
-                String name = input.nextLine();
-                try {
-                    //System.out.println(userDAO.getUsers());
-                    this.guest.searchForPersonalCollection(name);
-                    //System.out.println(name);
-                    this.searchResult = this.guest.getCurrentlySelectedPC().toString();
-                    //view.setCommand(Command.PCRESULT);
-                } catch (IOException e) {
-                    view.setCommand(Command.ERROR);
-                    view.handleCommand();
-                    view.setCommand(Command.GUEST);
-                    view.handleCommand();
-                } catch (IllegalArgumentException e){
-                    System.out.println("thing");
-                    view.setCommand(Command.ERROR);
-                    view.handleCommand();
-                    view.setCommand(Command.GUEST);
-                    view.handleCommand();
-                } catch (NullPointerException e){
-                    System.out.println("prob2");
-                    view.setCommand(Command.ERROR);
-                    view.handleCommand();
-                    view.setCommand(Command.GUEST);
-                    view.handleCommand();
-                }
-            }
+                case SIGNUP:
+                    this.username = input.nextLine();
+                    view.setCommand(Command.SIGNUPPASSWORD);
+                    break;
+                case SIGNUPPASSWORD:
+                    this.password = input.nextLine();
+                    try {
+                        this.user = guest.createAccount(this.username, this.password);
+                        this.collection = new PersonalCollection(this.username);
+                        this.user.setCollection(collection);
+                        view.setCommand(Command.SIGNEDINUSER);
+                    } catch (Exception e) {
+                        view.setCommand(Command.NEWACCOUNTERROR);
+                        view.handleCommand();
+                        view.setCommand(Command.GUEST);
+                    }
+                    break;
 
-            //USER COMMANDS
-            else if(view.getCommand().equals(Command.SIGNEDINUSER)){
-                String commandNum = input.nextLine();
-                if(commandNum.equals("1")){
-                    view.setCommand(Command.SEARCHTYPEDATABASE);
-                }
-                else if(commandNum.equals("2")){
-                    view.setCommand(Command.SEARCHCOLLECTION);
-                }
-                else if(commandNum.equals("3")){
-                    view.setCommand(Command.ADDTOCOLLECTION);
-                }
-                else if(commandNum.equals("4")){
-                    view.setCommand(Command.REMOVEFROMCOLLECTION);
-                }
-                else if(commandNum.equals("5")){
-                    view.setCommand(Command.EDITMARKSELECTION);
-                }
-                else if(commandNum.equals("6")){
-                    view.setCommand(Command.UNDO);
-                }
-                else if(commandNum.equals("7")){
-                    view.setCommand(Command.REDO);
-                }
-                else if(commandNum.equals("8")){
-                    view.setCommand(Command.IMPORTEXPORT);
-                }
-                else if(commandNum.equals("9")){
-                    view.setCommand(Command.CLOSING);
-                }
-                else{
-                    view.setCommand(Command.ERROR);
-                    view.handleCommand();
-                    view.setCommand(Command.SIGNEDINUSER);
-                    view.handleCommand();
-                }
-                view.handleCommand();
-            }
+                case BROWSEOTHERCOLLECTION:
+                    String name = input.nextLine();
+                    try {
+                        this.guest.searchForPersonalCollection(name);
+                        this.searchResult = this.guest.getCurrentlySelectedPC().toString();
+                        //view.setCommand(Command.PCRESULT);
+                    } catch (IOException|IllegalArgumentException|NullPointerException e) {
+                        view.setCommand(Command.ERROR);
+                        view.handleCommand();
+                        view.setCommand(Command.GUEST);
+                    }
+                    break;
+                
+                case SIGNEDINUSER:
+                    String commandNum = input.nextLine();
+                    switch(commandNum){
+                        case "1":
+                            view.setCommand(Command.SEARCHTYPEDATABASE);
+                            break;
+                        case "2":
+                            view.setCommand(Command.SEARCHCOLLECTION);
+                            break;
+                        case "3":
+                            view.setCommand(Command.ADDTOCOLLECTION);
+                            break;
+                        case "4":
+                            view.setCommand(Command.REMOVEFROMCOLLECTION);
+                            break;
+                        case "5":
+                            view.setCommand(Command.EDITMARKSELECTION);
+                            break;
+                        case "6":
+                            view.setCommand(Command.UNDO);
+                            break;
+                        case "7":
+                            view.setCommand(Command.REDO);
+                            break;
+                        case"8":
+                            view.setCommand(Command.IMPORTEXPORT);
+                            break;
+                        case "9":
+                            view.setCommand(Command.CLOSING);
+                            break;
+                        default:
+                            view.setCommand(Command.ERROR);
+                            view.handleCommand();
+                            view.setCommand(Command.SIGNEDINUSER);                        
+                    }
+                    break;
+                
+                case ADDTOCOLLECTION:
+                    break;
+                case EDITMARKSELECTION:
+                    break;
+                case ERROR:
+                    break;
+                case IMPORTEXPORT:
+                    break;
+                case NEWACCOUNTERROR:
+                    break;
+                case OTHERCOLLECTIONRESULT:
+                    break;
+                case REDO:
+                    break;
+                case REMOVEFROMCOLLECTION:
+                    break;
+                case SEARCHCOLLECTION, SEARCHDATABASE:
+                    boolean database = true;
+                    if(!view.getCommand().equals(Command.SEARCHDATABASE)){
+                        database = false;
+                    }
 
-            // - CHANGE SEARCH TYPE
-            else if(view.getCommand().equals(Command.SEARCHTYPEDATABASE) || view.getCommand().equals(Command.SEARCHTYPECOLLECTION)) {
-                String commandNum = input.nextLine();
-                Command newCommand = Command.SEARCHDATABASE;
-                if(!view.getCommand().equals(Command.SEARCHTYPEDATABASE)){
-                    newCommand = Command.SEARCHCOLLECTION;
-                }
+                    String search = input.nextLine();
+                    this.user.search(search);
+                    //this.user.search(search, database);
+                    break;
+
+                case SEARCHTYPECOLLECTION, SEARCHTYPEDATABASE:
+                    String searchNum = input.nextLine();
+                    Command newCommand = Command.SEARCHDATABASE;
+                    if(!view.getCommand().equals(Command.SEARCHTYPEDATABASE)){
+                        newCommand = Command.SEARCHCOLLECTION;
+                    }
             
-                if(commandNum.equals("1")){
-                    this.user.setSearchStrategy(new SearchBySeriesTitle());
-                    view.setCommand(newCommand);
-                }
-                else if(commandNum.equals("2")){
-                    this.user.setSearchStrategy(new SearchByPrincipleCharacter());
-                    view.setCommand(newCommand);
-                }
-                else if(commandNum.equals("3")){
-                    this.user.setSearchStrategy(new SearchByAuthor());
-                    view.setCommand(newCommand);
-                }
-                else if(commandNum.equals("4")){
-                    this.user.setSearchStrategy(new SearchByDescription());
-                    view.setCommand(newCommand);
-                }
-                else if(commandNum.equals("5")){
-                    view.setCommand(Command.SIGNEDINUSER);
-                }
-                else{
-                    view.setCommand(Command.ERROR);
-                    view.handleCommand();
-                    view.setCommand(Command.SIGNEDINUSER);
-                    view.handleCommand();
-                }
-                view.handleCommand();
-            }
+                    if(searchNum.equals("1")){
+                        this.user.setSearchStrategy(new SearchBySeriesTitle());
+                        view.setCommand(newCommand);
+                    }
+                    else if(searchNum.equals("2")){
+                        this.user.setSearchStrategy(new SearchByPrincipleCharacter());
+                        view.setCommand(newCommand);
+                    }
+                    else if(searchNum.equals("3")){
+                        this.user.setSearchStrategy(new SearchByAuthor());
+                        view.setCommand(newCommand);
+                    }
+                    else if(searchNum.equals("4")){
+                        this.user.setSearchStrategy(new SearchByDescription());
+                        view.setCommand(newCommand);
+                    }
+                    else if(searchNum.equals("5")){
+                        view.setCommand(Command.SIGNEDINUSER);
+                    }
+                    else{
+                        view.setCommand(Command.ERROR);
+                        view.handleCommand();
+                        view.setCommand(Command.SIGNEDINUSER);
+                    }
+                    break;
 
-            // - SEARCH
-            else if(view.getCommand().equals(Command.SEARCHDATABASE) || view.getCommand().equals(Command.SEARCHCOLLECTION)){
-                boolean database = true;
-                if(!view.getCommand().equals(Command.SEARCHDATABASE)){
-                    database = false;
-                }
-
-                String search = input.nextLine();
-                this.user.search(search);
-                //this.user.search(search, database);
-            }
-
-            // - ADD TO PERSONAL COLLECTION
-            else if(view.getCommand().equals(Command.ADDTOCOLLECTION)){
+                case UNDO:
+                    break;
+                    
+                case CLOSING:
+                    running = false;
+                    break;
+                default:
+                    break;
 
             }
-
-            // - REMOVE FROM PERSONAL COLLECTION
-            else if(view.getCommand().equals(Command.REMOVEFROMCOLLECTION)){
-
-            }
-
-            // - EDIT/MARK COMIC
-            else if(view.getCommand().equals(Command.EDITMARKSELECTION)){
-
-            }
-
-            // - UNDO COMMAND
-            else if(view.getCommand().equals(Command.UNDO)){
-
-            }
-
-            // - REDO COMMAND
-            else if(view.getCommand().equals(Command.REDO)){
-
-            }
-
-            // - IMPORT EXPORT
-            else if(view.getCommand().equals(Command.IMPORTEXPORT)){
-
-            }
-
-            else{
-                running = false;
-            }
+            view.handleCommand();
         }
     }
 
     public void init() throws IOException{
         ObjectMapper mockMapper = new ObjectMapper();
-        //mockMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        mockMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         this.userDAO = new UserFileDAO("src/data/users.json", mockMapper);
         this.mediator = new ComixLogin(this.userDAO);
         this.guest = new Guest(mediator);
