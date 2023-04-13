ad6723b0331dffd06168a299a0872f902fc60be4
AshRoushinko <Ashroushinko@gmail.com>
2023-04-12 20:59:38 -0400

    changed if-else statements to switch statements

--- src/main/java/com/swen/comix/view/PTUI.java
+++ src/main/java/com/swen/comix/view/PTUI.java
@@ -15,52 +15,42 @@ public class PTUI {
     }
 
     public void handleCommand(){
-        if(viewCommand.equals(Command.GUEST)){
-            System.out.println("Enter the number next to the command you want to select it:\n1.) Sign-in\n2.) Sign-up\n3.) Browse/Search Database\n4.) Browse Collections\n5.) Close Program");
-        }
-
-        else if(viewCommand.equals(Command.SIGNIN) || viewCommand.equals(Command.SIGNUP)){
-            System.out.println("Input Username:");
-        }
-
-        else if(viewCommand.equals(Command.SIGNINPASSWORD) || viewCommand.equals(Command.SIGNUPPASSWORD)){
-            System.out.println("Input Password:");
-        }
-
-        else if(viewCommand.equals(Command.SIGNINCOMPLETE)){
-            System.out.println("Signin Complete\n");
-        }
-
-        else if(viewCommand.equals(Command.SEARCHTYPEDATABASE) || viewCommand.equals(Command.SEARCHTYPECOLLECTION)){
-            System.out.println("\nHow Would You Like To Search:\n1.)Series Title\n2.)Principle Character\n3.)Author\n4.)Description\n5.)Go Back");
-        }
-
-        else if(viewCommand.equals(Command.SEARCHDATABASE) || viewCommand.equals(Command.SEARCHCOLLECTION)){
-            System.out.println("What Would You Like to Seach For?");
-        }
-        
-        else if(viewCommand.equals(Command.BROWSEOTHERCOLLECTION)){
-            System.out.println("Who's collection would you like to view?");
-        }
-
-        else if(viewCommand.equals(Command.OTHERCOLLECTIONRESULT)){
-            System.out.println("Their pc:\n");
-        }
-
-        else if(viewCommand.equals(Command.SIGNEDINUSER)){
-            System.out.println("Enter the number next to the command you want to select it:\n1.) Browse/Search Database\n2.) Search Personal Collection\n3.) Add to Personal Collection\n4.) Remove from Personal Collection\n5.) Edit/Mark Comic In Personal Collection\n6.) Undo Command\n7.) Redo Command\n8.) Import/Export Database\n9.) Close");
-        }
-
-        else if(viewCommand.equals(Command.NEWACCOUNTERROR)){
-            System.out.println("A User with this username and password already exists\nPlease Try Again:\n");
-        }
-
-        else if(viewCommand.equals(Command.CLOSING)){
-            System.out.println("Closing Program");
-        }
-
-        else{
-            System.out.println("Invalid Input\nPlease Try Again:\n");
+        switch(viewCommand){
+            case GUEST:
+                System.out.println("Enter the number next to the command you want to select it:\n1.) Sign-in\n2.) Sign-up\n3.) Browse/Search Database\n4.) Browse Collections\n5.) Close Program");
+                break;
+            case SIGNIN, SIGNUP:
+                System.out.println("Input Username:");
+                break;
+            case SIGNINPASSWORD, SIGNUPPASSWORD:
+                System.out.println("Input Password:");
+                break;
+            case SIGNINCOMPLETE:
+                System.out.println("Signin Complete\n");
+                break;
+            case SEARCHTYPEDATABASE, SEARCHTYPECOLLECTION:
+                System.out.println("\nHow Would You Like To Search:\n1.)Series Title\n2.)Principle Character\n3.)Author\n4.)Description\n5.)Go Back");
+                break;
+            case SEARCHDATABASE, SEARCHCOLLECTION:
+                System.out.println("What Would You Like to Seach For?");
+                break;
+            case BROWSEOTHERCOLLECTION:
+                System.out.println("Who's collection would you like to view?");
+                break;
+            case OTHERCOLLECTIONRESULT:
+                System.out.println("Their pc:\n");
+                break;
+            case SIGNEDINUSER:
+                System.out.println("Enter the number next to the command you want to select it:\n1.) Browse/Search Database\n2.) Search Personal Collection\n3.) Add to Personal Collection\n4.) Remove from Personal Collection\n5.) Edit/Mark Comic In Personal Collection\n6.) Undo Command\n7.) Redo Command\n8.) Import/Export Database\n9.) Close");
+                break;
+            case NEWACCOUNTERROR:
+                System.out.println("A User with this username and password already exists\nPlease Try Again:\n");
+                break;
+            case CLOSING:
+                System.out.println("Closing Program");
+                break;
+            default:
+                System.out.println("Invalid Input\nPlease Try Again:\n");
         }
     }
 
