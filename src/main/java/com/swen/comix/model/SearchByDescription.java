35cf721c75bf19bd725f34dd30c2db97c7339040
jjb7866 <jjb7866@rit.edu>
2023-04-14 16:32:51 -0400

    added to search by description construct

--- src/main/java/com/swen/comix/model/SearchByDescription.java
+++ src/main/java/com/swen/comix/model/SearchByDescription.java
@@ -7,18 +7,24 @@ import com.swen.comix.db.*;
 
 /**
  * @Author Angela and Joe
- * Returns arraylist of comicbooks from search results looking for specific Description
+ *         Returns arraylist of comicbooks from search results looking for
+ *         specific Description
  */
 public class SearchByDescription implements SearchStrategy {
     private PersonalCollection personalCollection;
+    private Database db;
 
     public SearchByDescription(PersonalCollection personalCollection) {
         this.personalCollection = personalCollection;
     }
 
+    public SearchByDescription(Database db) {
+        this.db = db;
+    }
+
     /**
      * @param toBeSearched String - contains the description to be searched
-     * @param isSearchDb boolean - contains if you are searching on db or on pc 
+     * @param isSearchDb   boolean - contains if you are searching on db or on pc
      */
     @Override
     public ArrayList<ComicBook> algorithm(String toBeSearched, boolean isSearchDb) throws Exception {
@@ -35,22 +41,24 @@ public class SearchByDescription implements SearchStrategy {
     /**
      * 
      * @param toBeSearched String - the description
-     * @return ArrayList<ComicBook> - arraylist of comicbooks returned after finding matching description
+     * @return ArrayList<ComicBook> - arraylist of comicbooks returned after finding
+     *         matching description
      * @throws Exception
-     */  
+     */
     private ArrayList<ComicBook> searchOnDb(String toBeSearched) throws Exception {
 
-        iDatabase db = new Database();
-
-        ResultSet res = db.getTable("SELECT * FROM comics WHERE description = '" + toBeSearched + "'");
+        ResultSet res = db.getTable("SELECT * FROM comics WHERE description LIKE '%" + toBeSearched + "%'");
 
         return db.resToArrayList(res);
     }
 
     /**
-     * Searches on PersonalCollection to return an arraylist of comic books based on toBeSearched
+     * Searches on PersonalCollection to return an arraylist of comic books based on
+     * toBeSearched
+     * 
      * @param toBeSearched String - containing the description to be searched
-     * @return ArrayList <ComicBook> - contains the matching comic books with description 
+     * @return ArrayList <ComicBook> - contains the matching comic books with
+     *         description
      */
     private ArrayList<ComicBook> searchOnPC(String toBeSearched) {
         ArrayList<ComicBook> comics = new ArrayList<>();
