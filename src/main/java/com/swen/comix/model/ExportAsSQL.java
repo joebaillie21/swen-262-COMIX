c9ba6886df67239a6b66aa0773c90b19e0ccca9e
joebaillie21 <jjb7866@g.rit.edu>
2023-04-15 17:05:51 -0400

    sql export additions

--- src/main/java/com/swen/comix/model/ExportAsSQL.java
+++ src/main/java/com/swen/comix/model/ExportAsSQL.java
@@ -6,8 +6,15 @@ public class ExportAsSQL implements Exporter {
 
     @Override
     public String export(ArrayList<ComicBook> comicBooks) {
+        String psuedoPath = """
+                INSERT INTO comics (series_title, volume_number, issue_number, publication_date, author, publisher, principle_character)
+                        VALUES
+                """;
+        for (ComicBook c : comicBooks) {
 
-        return "";
+        }
+        return null;
+
     }
 
 }
