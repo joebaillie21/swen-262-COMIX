51154ca461a814e11c3a20f1893caf1795da1561
Angela Ngo <ngo.angela0@gmail.com>
2023-04-16 13:51:39 -0400

    fixed the gaps thing -- now ready to merge after rebasing

--- src/main/java/com/swen/comix/model/SearchForGaps.java
+++ src/main/java/com/swen/comix/model/SearchForGaps.java
@@ -69,10 +69,10 @@ public class SearchForGaps implements SearchStrategy{
      * This sorts the arraylist by series title, volume number and issue number 
      * @return ArrayList<ComicBook> - returns sorted arraylist sorted by series title, volume number and issue number
      */
-    private ArrayList<ComicBook> sort(){
+    public ArrayList<ComicBook> sort(){
         Comparator<ComicBook> bySeriesTitle = (book1, book2) -> book1.getSeriesTitle().compareTo(book2.getSeriesTitle());
-        Comparator<ComicBook> byVolumeNum = (book1, book2) -> Integer.compare(book1.getVolNum(), book2.getVolNum());
-        Comparator<ComicBook> byIssueNum = (book1, book2) -> Integer.compare(book1.getIssueNum(), book2.getIssueNum());
+        Comparator<ComicBook> byVolumeNum = (book1, book2) -> Integer.compare(Integer.valueOf(book1.getVolNum()), Integer.valueOf(book2.getVolNum()));
+        Comparator<ComicBook> byIssueNum = (book1, book2) -> Integer.compare(Integer.valueOf(book1.getIssueNumber()), Integer.valueOf(book2.getIssueNumber()));
 
         Comparator<ComicBook> chain = bySeriesTitle
             .thenComparing(byVolumeNum)
@@ -81,13 +81,13 @@ public class SearchForGaps implements SearchStrategy{
         Collections.sort(runs,chain);
         return (ArrayList<ComicBook>)runs;
 
+        // write tests that test that this change persists in the object 
+
     }
+
     
-    /**
-     * Helper method for the overall searchOnPC() method, splits the collection before finding the gaps 
-     * @return ArrayList<ArrayList<ComicBook>> - a list of comics split by type of series 
-     */
-    private ArrayList<ArrayList<ComicBook>> splitBySeries(){
+
+    public ArrayList<ArrayList<ComicBook>> splitBySeries(){
         ArrayList<ArrayList<ComicBook>> series = new ArrayList<>(); 
         ArrayList<ComicBook> serie = new ArrayList<>(); 
 
@@ -98,6 +98,7 @@ public class SearchForGaps implements SearchStrategy{
                 serie.add(runs.get(i));
         
             }else if(!(title.equals(runs.get(i).getSeriesTitle()))){
+                //System.out.println(runs.get(i).getSeriesTitle());
                 series.add(serie); 
                 serie = new ArrayList<>(); 
                 serie.add(runs.get(i));
@@ -109,23 +110,18 @@ public class SearchForGaps implements SearchStrategy{
         return series; 
     }
 
-    /**
-     * Helper method for the overall searchOnPC() method, splits the collection by volume before finding the gaps 
-     * @param sortedByTitle - ArrayList<ArrayList<ComicBook>> the list of comics in type of series 
-     * @return ArrayList<ArrayList<ComicBook>> - the arraylist containing comics split by series and by volume number
-     */
-    private ArrayList<ArrayList<ComicBook>> splitByVolNum(ArrayList<ArrayList<ComicBook>> sortedByTitle){
+    public ArrayList<ArrayList<ComicBook>> splitByVolNum(ArrayList<ArrayList<ComicBook>> sortedByTitle){
         ArrayList<ArrayList<ComicBook>> volNums = new ArrayList<>(); 
         ArrayList<ComicBook> vol = new ArrayList<>(); 
 
         for(int i = 0; i < sortedByTitle.size(); i ++){
-            int volNum = sortedByTitle.get(i).get(0).getVolNum();
+            String volNum = sortedByTitle.get(i).get(0).getVolNum();
 
             for( int k = 0; k < sortedByTitle.get(i).size(); k++){
                 ComicBook cb = sortedByTitle.get(i).get(k);
-                if(volNum == cb.getVolNum()){
+                if(volNum.equals(cb.getVolNum())){
                     vol.add(cb);
-                }else if(volNum != cb.getVolNum()){
+                }else if(!volNum.equals(cb.getVolNum())){
                     volNums.add(vol);
                     vol = new ArrayList<>(); 
                     vol.add(cb);
@@ -140,25 +136,45 @@ public class SearchForGaps implements SearchStrategy{
 
     /**
      * this is supposed to only return the series vol issues if considered a run
-     * 
-     * @param sortedByVolNum - ArrayList<ArrayList<ComicBook>> - the arraylist containing comics split by series and by volume number
-     * @return ArrayList<ArrayList<ComicBook>> this contains the comics that are considered a run with gaps in it 
+     * @param sortedByVolNum
+     * @return
      */
-    private ArrayList<ArrayList<ComicBook>> splitByIssueNum(ArrayList<ArrayList<ComicBook>> sortedByVolNum){
+    public ArrayList<ArrayList<ComicBook>> splitByIssueNum(ArrayList<ArrayList<ComicBook>> sortedByVolNum){
         ArrayList<ArrayList<ComicBook>> consecutiveNums = new ArrayList<>(); 
         ArrayList<ComicBook> issues = new ArrayList<>(); 
-
-        for(int i = 0; i < sortedByVolNum.size(); i++){ // get one volume 
-            int issueNum = sortedByVolNum.get(i).get(0).getIssueNum();
-            for(int k = 0 ; k < sortedByVolNum.get(i).size(); k++){ // get the issue numbers from the volume 
-                ComicBook cb = sortedByVolNum.get(i).get(k);
-                if(cb.getIssueNum())
-            }
-        }
         
+        for(int i = 0; i < sortedByVolNum.size(); i++){
+            //System.out.println(sortedByVolNum.get(i).size());
+            String issueNum = sortedByVolNum.get(i).get(0).getIssueNumber();
+            int count = 0; 
+            int missing = 0;  
+            for(int k = 0; k < sortedByVolNum.get(i).size(); k++){ //basic
+                ComicBook cb = sortedByVolNum.get(i).get(k);
+                String currIssue = cb.getIssueNumber();  
+                if(issueNum.equals(currIssue)){
+                    count++; 
+                    issues.add(cb);
+                    int prevNum = Integer.parseInt(issueNum);
+                    issueNum = String.valueOf(prevNum+1); 
+                }else if(!issueNum.equals(currIssue)){
+                    if(missing != MAX_MISSING){
+                        missing += Integer.parseInt(currIssue) - Integer.parseInt(issueNum); 
+                        issueNum = String.valueOf(Integer.parseInt(currIssue)); //currIssue;
+                        k--;
+                    }
+                }
+            }
+
+            if(MAX_MISSING >= missing && count >= MIN_CONSEC_ISSUES){ // if there are issues in there meaning its consec as well as the count is great 
+                consecutiveNums.add(issues); // TO DO: smush them all together into one array list  
+            }
+            issues = new ArrayList<>(); // reset 
+        }
 
         return consecutiveNums; 
     }
+
+
 
     public static void main(String []  args){
         PersonalCollection pc = new PersonalCollection("meow"); 
@@ -171,26 +187,27 @@ public class SearchForGaps implements SearchStrategy{
         ArrayList<Author> author = new ArrayList<>(); 
         author.add(new Author("lalo"));
         author.add(new Author("papa"));  
-        ComicBook cb1 = new ComicBookComponent(p1, title1, 1, 2, pubdate, author, null,  "example"  );
-        ComicBook cb2 = new ComicBookComponent(p2, title2, 1, 1, pubdate, author, null,  "example"  );
-        ComicBook cb3 = new ComicBookComponent(p1, title1, 4, 2, pubdate, author, null,  "example"  );
-        ComicBook cb4 = new ComicBookComponent(p2, title2, 3, 1, pubdate, author, null,  "example"  );
-        ComicBook cb5 = new ComicBookComponent(p1, title1, 1, 3, pubdate, author, null,  "example"  );
-        ComicBook cb6 = new ComicBookComponent(p2, title2, 3, 3, pubdate, author, null,  "example"  );
+        ComicBook cb1 = new ComicBookComponent(p1, title1, "1", "2", pubdate, author, null,  "example"  );
+        ComicBook cb2 = new ComicBookComponent(p2, title2, "1", "1", pubdate, author, null,  "example"  );
+        ComicBook cb3 = new ComicBookComponent(p1, title1, "4", "2", pubdate, author, null,  "example"  );
+        ComicBook cb4 = new ComicBookComponent(p2, title2, "3", "1", pubdate, author, null,  "example"  );
+        ComicBook cb5 = new ComicBookComponent(p1, title1, "1", "3", pubdate, author, null,  "example"  );
+        ComicBook cb6 = new ComicBookComponent(p2, title2, "3", "3", pubdate, author, null,  "example"  );
         // the run 
-        ComicBook cb7 = new ComicBookComponent(p1, title1, 1, 1, pubdate, author, null,  "example"  );
-        ComicBook cb8 = new ComicBookComponent(p1, title1, 1, 4, pubdate, author, null,  "example"  );
-        ComicBook cb9 = new ComicBookComponent(p1, title1, 1, 5, pubdate, author, null,  "example"  );
-        ComicBook cb10 = new ComicBookComponent(p1, title1, 1, 6, pubdate, author, null,  "example"  );
-        ComicBook cb11 = new ComicBookComponent(p1, title1, 1, 7, pubdate, author, null,  "example"  );
-        ComicBook cb12 = new ComicBookComponent(p1, title1, 1, 8, pubdate, author, null,  "example"  );
-        ComicBook cb13 = new ComicBookComponent(p1, title1, 1, 9, pubdate, author, null,  "example"  );
-        ComicBook cb14 = new ComicBookComponent(p1, title1, 1, 10, pubdate, author, null,  "example"  );
-        ComicBook cb15 = new ComicBookComponent(p1, title1, 1, 11, pubdate, author, null,  "example"  );
-        ComicBook cb16 = new ComicBookComponent(p1, title1, 1, 12, pubdate, author, null,  "example"  );
-        ComicBook cb17 = new ComicBookComponent(p1, title1,1 , 13, pubdate, author, null,  "example"  );
-        ComicBook cb18 = new ComicBookComponent(p1, title1, 1, 14, pubdate, author, null,  "example"  );
-        ComicBook cb19 = new ComicBookComponent(p1, title1,1 , 15, pubdate, author, null,  "example"  );
+        ComicBook cb7 = new ComicBookComponent(p1, title1, "1", "1", pubdate, author, null,  "example"  );
+        ComicBook cb8 = new ComicBookComponent(p1, title1, "1", "4", pubdate, author, null,  "example"  );
+        ComicBook cb9 = new ComicBookComponent(p1, title1, "1", "5", pubdate, author, null,  "example"  );
+        ComicBook cb10 = new ComicBookComponent(p1, title1, "1", "6", pubdate, author, null,  "example"  );
+        ComicBook cb11 = new ComicBookComponent(p1, title1, "1", "7", pubdate, author, null,  "example"  );
+        ComicBook cb12 = new ComicBookComponent(p1, title1, "1", "8", pubdate, author, null,  "example"  );
+        ComicBook cb13 = new ComicBookComponent(p1, title1, "1", "9", pubdate, author, null,  "example"  );
+        ComicBook cb14 = new ComicBookComponent(p1, title1, "1", "10", pubdate, author, null,  "example"  );
+        ComicBook cb15 = new ComicBookComponent(p1, title1, "1", "11", pubdate, author, null,  "example"  );
+        ComicBook cb16 = new ComicBookComponent(p1, title1, "1", "12", pubdate, author, null,  "example"  );
+        ComicBook cb17 = new ComicBookComponent(p1, title1, "2", "12", pubdate, author, null,  "example"  );
+        ComicBook cb18 = new ComicBookComponent(p1, title1, "1", "14", pubdate, author, null,  "example"  );
+        ComicBook cb19 = new ComicBookComponent(p1, title1,"1" , "15", pubdate, author, null,  "example"  );
+        ComicBook cb20 = new ComicBookComponent(p1, title1,"1" , "13", pubdate, author, null,  "example"  );
         
         pc.add(cb16);
         pc.add(cb1);
@@ -209,6 +226,7 @@ public class SearchForGaps implements SearchStrategy{
         pc.add(cb18);
         pc.add(cb19);
         pc.add(cb17);
+        pc.add(cb20);
 
 
         SearchForGaps sfg = new SearchForGaps(pc);
@@ -230,23 +248,23 @@ public class SearchForGaps implements SearchStrategy{
 
         ArrayList<ArrayList<ComicBook>> splitVolNum = sfg.splitByVolNum(splitSeries);
         
-        for(int i = 0; i < splitVolNum.size(); i++){
-            System.out.println("meo");
-            for(int k = 0; k < splitVolNum.get(i).size(); k++){
-                System.out.println(splitVolNum.get(i).get(k).toString()); 
-            }
-        }
-
-
-        // ArrayList<ArrayList<ComicBook>> splitIssueNum = sfg.splitByIssueNum(splitVolNum); 
-
-        // System.out.println(splitIssueNum.size());
-        // for(int i = 0; i < splitIssueNum.size(); i++){
+        // for(int i = 0; i < splitVolNum.size(); i++){
         //     System.out.println("meo");
-        //     for(int k = 0; k < splitIssueNum.get(i).size(); k++){
-        //         System.out.println(splitIssueNum.get(i).get(k).toString()); 
+        //     for(int k = 0; k < splitVolNum.get(i).size(); k++){
+        //         System.out.println(splitVolNum.get(i).get(k).toString()); 
         //     }
         // }
+
+
+        ArrayList<ArrayList<ComicBook>> splitIssueNum = sfg.splitByIssueNum(splitVolNum); 
+
+        System.out.println(splitIssueNum.size());
+        for(int i = 0; i < splitIssueNum.size(); i++){
+            System.out.println("meo");
+            for(int k = 0; k < splitIssueNum.get(i).size(); k++){
+                System.out.println(splitIssueNum.get(i).get(k).toString()); 
+            }
+        }
     
         
 
